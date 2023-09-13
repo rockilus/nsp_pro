fafacc2660e750f54014c6b1d0050c016915030b
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2023-09-13 14:35:15 +0200

    Rename folder to ConstraintConfig.

--- frontend/src/containers/Draft.tsx
+++ frontend/src/containers/Draft.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import Box from "@mui/material/Box";
 
-import ConstraintConfig from "../components/ConfigSentences/ConstraintConfig";
+import ConstraintConfig from "../components/ConstraintConfig/ConstraintConfig";
 import ShiftConfig from "../components/ConfigTables/ShiftConfig";
 import WorkerConfig from "../components/ConfigTables/WorkerConfig";
 
