72df5d28ef0bdc4fa78b8a19fd642839cbb069a0
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2023-09-13 12:08:06 +0200

    Move constraint transformation functions into a new module "constraint_transform".

---
+++ backend/src/constraint_transform/constraint_utils.py
@@ -0,0 +1,72 @@
+from typing import Dict, List
+
+
+def get_var_value(constraint: Dict, constraint_variables: List[Dict]) -> str:
+    variables = {
+        "worker": [],
+        "day": [],
+        "shift": ["off", "morning", "afternoon", "night"],
+    }
+    if constraint["constraint_type"] == "order":
+        constraint_var = next(
+            (d for d in constraint_variables if d["intra"] is True), {}
+        )
+        var_value = variables[constraint_var["param"]][constraint_var["value"]]
+    else:
+        constraint_var = next(
+            (d for d in constraint_variables if d["operator"] == "equal"),
+            {},
+        )
+        var_value = variables[constraint_var["param"]][constraint_var["value"]]
+    return var_value
+
+
+def get_ref_var_value(
+    constraint: Dict, constraint_variables: List[Dict]
+) -> str:
+    ref_var_value = ""
+    variables = {
+        "worker": [],
+        "day": [],
+        "shift": ["off", "morning", "afternoon", "night"],
+    }
+    if constraint["constraint_type"] == "sum":
+        constraint_var = next(
+            (d for d in constraint_variables if d["operator"] == "interval"),
+            {},
+        )
+        ref_var_value = ""
+        if constraint_var["value"] == 0 and constraint_var["interval"] == 7:
+            ref_var_value = "week"
+    elif constraint["constraint_type"] == "sequence":
+        constraint_var = next(
+            (d for d in constraint_variables if d["operator"] == "equal"),
+            {},
+        )
+        ref_var_value = variables[constraint_var["param"]][
+            constraint_var["value"]
+        ]
+    elif constraint["constraint_type"] == "order":
+        constraint_var = next(
+            (d for d in constraint_variables if d["operator"] == "offset"),
+            {},
+        )
+        ref_var_value = constraint_var["interval"]
+
+    return ref_var_value
+
+
+def get_other_var_value(constraint_variables: List[Dict]) -> str:
+    variables = {
+        "worker": [],
+        "day": [],
+        "shift": ["off", "morning", "afternoon", "night"],
+    }
+    constraint_var = next(
+        (d for d in constraint_variables if d["intra"] is True),
+        {},
+    )
+    other_var_value = variables[constraint_var["param"]][
+        constraint_var["other_value"]
+    ]
+    return other_var_value
