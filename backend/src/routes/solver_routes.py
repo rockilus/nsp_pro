bf167476e2cb74170b4f45fe34d22c68b2260b9b
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2023-10-02 19:28:57 +0200

    Feed inputs from database to engine instead of dummy.

--- backend/src/routes/solver_routes.py
+++ backend/src/routes/solver_routes.py
@@ -1,29 +1,31 @@
+from datetime import timedelta
+from typing import List
+
 from fastapi import APIRouter
 
+from core.coverage import Coverage as CoverageCore
 from engine import Coverage, Custom, Engine, Inputs, ShiftDemand, VariableSpace
+from scripts.setup_database import coverage_db, shift_db, worker_db
 
 router = APIRouter()
 
 
 @router.get("/solver")
 def solver():
+    workers = worker_db.get_workers()
+    shifts = shift_db.get_shifts()
+    coverages = coverage_db.get_coverages()
+    start_date, end_date = get_start_end_dates(coverages)
     engine = Engine()
     variable_space = VariableSpace(
-        workers=["w0", "w1", "w2", "w3", "w4", "w5", "w6"],
-        start_date="2024-10-02",
-        end_date="2024-10-15",
-        shifts=["s0", "s1", "s2", "s3"],
+        workers=[worker.id for worker in workers],
+        start_date=start_date,
+        end_date=end_date,
+        shifts=[shift.id for shift in shifts],
     )
+    shift_demands = build_shift_demands(coverages)
     # pylint: disable=R0801
-    coverage = Coverage(
-        coverage=[
-            ShiftDemand(
-                date="2024-10-08",
-                shift_id="s1",
-                quantity=3,
-            ),
-        ]
-    )
+    coverage = Coverage(shift_demands)
     requests = []
     fix_assignments = []
     custom = Custom(custom_constraints=[])
@@ -40,3 +42,27 @@ def solver():
     print(outputs)
 
     return {"msg": "all good"}
+
+
+def get_start_end_dates(coverages: list[CoverageCore]) -> tuple[str, str]:
+    date_format = "%Y-%m-%d"
+    start_date = min(c.date_start for c in coverages)
+    end_date = max(c.date_end for c in coverages)
+    return start_date.strftime(date_format), end_date.strftime(date_format)
+
+
+def build_shift_demands(coverages: List[CoverageCore]) -> List[ShiftDemand]:
+    shift_demands = []
+    for coverage in coverages:
+        for day in range((coverage.date_end - coverage.date_start).days + 1):
+            date = (coverage.date_start + timedelta(days=day)).strftime("%Y-%m-%d")
+            for shift_demand in coverage.shift_demands:
+                if shift_demand.day_index == day:
+                    shift_demands.append(
+                        ShiftDemand(
+                            date=date,
+                            shift_id=shift_demand.shift_id,
+                            quantity=shift_demand.quantity,
+                        )
+                    )
+    return shift_demands
