fa2d25c6ef1cae15c1aa35128914eb1947d52b66
FelipeKha <62256236+FelipeKha@users.noreply.github.com>
2023-10-06 11:06:06 +0200

    Change Shift Demands' Coverage input tab NPT 74 (#45)
    * Change Shift Demands' Coverage input tab.
    
    * Fix CoveragePanel select error.
    
    ---------
    
    Co-authored-by: Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>

--- frontend/src/components/Coverage/CoveragePanel.tsx
+++ frontend/src/components/Coverage/CoveragePanel.tsx
@@ -1,114 +1,134 @@
-import React, { Component, useEffect, useState } from 'react';
-import CoverageEditableView from './CoverageEditableView';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { CoverageT, ShiftT } from './types';
-import { useCoverageStore } from '../../stores/coverageStore';
+import React, { Component, useEffect, useState } from "react";
+import CoverageEditableView from "./CoverageEditableView";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { CoverageT, ShiftT } from "./types";
+import { useCoverageStore } from "../../stores/coverageStore";
 
 type CoveragePanelProps = {
-    shifts: ShiftT[];
+  shifts: ShiftT[];
 };
 
-const CoveragePanel: React.FC<CoveragePanelProps> = ({shifts}) => {
-    // remote interactions via stores
-    const coverages = useCoverageStore(state => state.coverages);
-    const fetchCoverages = useCoverageStore(state => state.fetchCoverages);
-    const addCoverage = useCoverageStore(state => state.addCoverage);
-    const updateCoverage = useCoverageStore(state => state.updateCoverage);
-    
-    
-    // local states via states
-    const [selectedCoverage, setSelectedCoverage] = useState<CoverageT | undefined>(undefined);
-    const [isNewCoverage, setIsNewCoverage] = useState<boolean>(false);
+const CoveragePanel: React.FC<CoveragePanelProps> = ({ shifts }) => {
+  // remote interactions via stores
+  const coverages = useCoverageStore((state) => state.coverages);
+  const fetchCoverages = useCoverageStore((state) => state.fetchCoverages);
+  const addCoverage = useCoverageStore((state) => state.addCoverage);
+  const updateCoverage = useCoverageStore((state) => state.updateCoverage);
 
-    // Fetch coverages from the API on mount
-    useEffect(() => {
-        fetchCoverages();
-    }, [fetchCoverages]);
+  // local states via states
+  const [selectedCoverage, setSelectedCoverage] = useState<
+    CoverageT | undefined
+  >(undefined);
+  const [isNewCoverage, setIsNewCoverage] = useState<boolean>(false);
+  const [newCoverage, setNewCoverage] = useState<CoverageT | undefined>(
+    undefined
+  );
 
-    // handlers for callbacks
-    const handleCoverageChange = async (updatedCoverage: CoverageT) => {
-        if (isNewCoverage) {
-            const newCoverage = await addCoverage(updatedCoverage);
-            setSelectedCoverage(newCoverage);
-            setIsNewCoverage(false);  // Reset this flag after addingaddCoverage(updatedCoverage);
-        } else {
-            updateCoverage(updatedCoverage);
-        }
-    };
+  // Fetch coverages from the API on mount
+  useEffect(() => {
+    fetchCoverages();
+  }, [fetchCoverages]);
 
-    const handleSelectCoverage = (coverage: CoverageT) => {
-        setSelectedCoverage(coverage);
-        setIsNewCoverage(false);
-    };
+  // handlers for callbacks
+  const handleCoverageChange = async (updatedCoverage: CoverageT) => {
+    if (isNewCoverage) {
+      const newCoverage = await addCoverage(updatedCoverage);
+      setSelectedCoverage(newCoverage);
+      setIsNewCoverage(false); // Reset this flag after addingaddCoverage(updatedCoverage);
+    } else {
+      updateCoverage(updatedCoverage);
+    }
+  };
 
-    const handleCreateNewCoverage = () => {
-        const now = new Date();
-        const startOfDayUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0));
-        const newCoverage: CoverageT = {
-            id: `id-${Date.now()}`, // Temporary unique ID, replace with real ID from the backend if needed
-            name: '',
-            dateStart: startOfDayUTC,
-            dateEnd:startOfDayUTC,
-            shiftDemands: []
-        };
-        setSelectedCoverage(newCoverage);
-        setIsNewCoverage(true);
-    };
-    
+  const handleSelectCoverage = (coverage: CoverageT) => {
+    setSelectedCoverage(coverage);
+    setIsNewCoverage(false);
+  };
 
-return (
-    <Box display="flex" flexDirection="column" gap={1}>
-        <Typography variant="h4" align="left">
-            Coverage Configuration
-        </Typography>
-        {/* Render a list of coverages */}
-        <Box display="flex" flexDirection="row" gap={1}>
-            <Select
-                value={selectedCoverage?.id || "initial"}
-                onChange={(e) => {
-                    const selectedId = e.target.value as string;
-                    const selectedCoverage = coverages.find(c => c.id === selectedId);
-                    if (selectedCoverage) {
-                        handleSelectCoverage(selectedCoverage);
-                    }
-                }}
-                variant="outlined"
-                size="small"
-            >
-                <MenuItem value="initial" disabled>
-                    Select Coverage
-                </MenuItem>
-                {coverages.map(coverage => (
-                    <MenuItem key={coverage.id} value={coverage.id}>
-                        {coverage.name}
-                    </MenuItem>
-                ))}
-            </Select>
-
-            {/* Button to add a new coverage */}
-            <Button 
-                variant="contained" 
-                color="primary" 
-                startIcon={<AddIcon/>}
-                onClick={handleCreateNewCoverage}
-            >
-                Add New Coverage
-            </Button>
-        </Box>
-
-        {selectedCoverage && (
-            <CoverageEditableView
-                coverage={selectedCoverage}
-                onChange={handleCoverageChange}
-                shifts={shifts} />
-        )}
-    </Box>
+  const handleCreateNewCoverage = () => {
+    const now = new Date();
+    const startOfDayUTC = new Date(
+      Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate(),
+        0,
+        0,
+        0,
+        0
+      )
     );
-}
+    const newCoverage: CoverageT = {
+      id: `id-${Date.now()}`, // Temporary unique ID, replace with real ID from the backend if needed
+      name: "",
+      dateStart: startOfDayUTC,
+      dateEnd: startOfDayUTC,
+      shiftDemands: [],
+    };
+    setNewCoverage(newCoverage);
+    setSelectedCoverage(newCoverage);
+    setIsNewCoverage(true);
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" gap={1}>
+      <Typography variant="h4" align="left">
+        Coverage Configuration
+      </Typography>
+      {/* Render a list of coverages */}
+      <Box display="flex" flexDirection="row" gap={1}>
+        <Select
+          value={selectedCoverage?.id || "initial"}
+          onChange={(e) => {
+            const selectedId = e.target.value as string;
+            const selectedCoverage = coverages.find((c) => c.id === selectedId);
+            if (selectedCoverage) {
+              handleSelectCoverage(selectedCoverage);
+            }
+          }}
+          variant="outlined"
+          size="small"
+        >
+          <MenuItem value="initial" disabled>
+            Select Coverage
+          </MenuItem>
+          {isNewCoverage && (
+            <MenuItem value={newCoverage?.id || ""}>
+              {newCoverage?.name || "New Coverage"}
+            </MenuItem>
+          )}
+          {coverages.map((coverage) => (
+            <MenuItem key={coverage.id} value={coverage.id}>
+              {coverage.name}
+            </MenuItem>
+          ))}
+        </Select>
+
+        {/* Button to add a new coverage */}
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddIcon />}
+          onClick={handleCreateNewCoverage}
+        >
+          Add New Coverage
+        </Button>
+      </Box>
+
+      {selectedCoverage && (
+        <CoverageEditableView
+          coverage={selectedCoverage}
+          onChange={handleCoverageChange}
+          shifts={shifts}
+        />
+      )}
+    </Box>
+  );
+};
 
 export default CoveragePanel;
