aff274a68b02d96e6f472fd4c2d5b91d603ac4c5
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2023-10-26 11:39:00 +0200

    Run make format.

--- backend/src/routes/schedule_routes.py
+++ backend/src/routes/schedule_routes.py
@@ -38,9 +38,7 @@ def solver() -> ScheduleMessage:
         if coverage_selector.coverage_id == "":
             coverages.append(None)
             continue
-        coverage = coverage_db.get_coverage_by_id(
-            coverage_selector.coverage_id
-        )
+        coverage = coverage_db.get_coverage_by_id(coverage_selector.coverage_id)
         coverages.append(coverage)
     fixed_assignments = fixed_assignment_db.get_fixed_assignments()
     requests = request_db.get_requests()
