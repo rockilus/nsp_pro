c5aea4d9424950a3c9f6263fcf26af61326b7672
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2023-10-26 15:02:02 +0200

    Remove legacy constraint code.

--- frontend/src/components/Constraint/types.ts
+++ frontend/src/components/Constraint/types.ts
@@ -17,24 +17,3 @@ export type BuildBlockT = {
   name: string;
   value: string | number;
 };
-
-// export type ConstraintT = {
-//   id: string;
-//   name: string;
-//   type: string;
-//   hard: boolean;
-//   priority: string;
-//   active: boolean;
-// };
-
-// export type ConstraintParamsT = {
-//   quantity: number;
-//   quantified_variable: string;
-//   var_value: string;
-//   timing: string;
-//   operator: string;
-//   reference_variable: string;
-//   ref_var_value: string;
-//   other_variable: string;
-//   other_var_value: string;
-// };
