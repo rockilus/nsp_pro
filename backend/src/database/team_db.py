5c395d71e8d0eedb89aafd7ffada91940da48971
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-03-28 13:55:12 +0100

    Add error handling and logging to team_db.

--- backend/src/database/team_db.py
+++ backend/src/database/team_db.py
@@ -4,8 +4,16 @@ from bson import ObjectId
 
 from core.team import Team
 from database.db import DB
+from errors import (
+    handle_create_core_object_error,
+    handle_create_document_error,
+    handle_delete_document_error,
+    handle_get_document_error,
+    handle_save_document_error,
+)
 from models import Team as TeamDocument
 from models import User as UserDocument
+from services.logging import log_info
 
 
 class TeamDB:
@@ -13,61 +21,104 @@ class TeamDB:
         self.db = db
 
     def create_team(self, team: Team) -> Team:
-        team_d = TeamDocument(
-            id=str(ObjectId()),
-            team_members=team.team_members,
-            team_leaders=team.team_leaders,
-        )
-        team_saved = team_d.save()
-        return _from_mongo_team(team_saved)
+        team_doc = core_to_doc_team(team)
+        team_doc.id = str(ObjectId())
+        try:
+            team_saved = team_doc.save()
+        except Exception as e:
+            log_info(f"Failed to save team to database: {e}")
+            handle_save_document_error(e)
+        return doc_to_core_team(team_saved)
 
-    def get_teams(
-        self,
-    ) -> List[Team]:
-        # pylint: disable=no-member
-        teams = TeamDocument.objects.all()  # type: ignore
-        return [_from_mongo_team(t) for t in list(teams)]
+    def get_teams(self) -> List[Team]:
+        try:
+            # pylint: disable=no-member
+            teams = TeamDocument.objects.all()  # type: ignore
+        except Exception as e:
+            log_info(f"Failed to get teams from database: {e}")
+            handle_get_document_error(e)
+        return [doc_to_core_team(t) for t in list(teams)]
 
     def get_team_by_id(self, team_id: str) -> Team:
-        # pylint: disable=no-member
-        team = TeamDocument.objects.get(id=team_id)  # type: ignore
-        return _from_mongo_team(team)
+        try:
+            # pylint: disable=no-member
+            team = TeamDocument.objects.get(id=team_id)  # type: ignore
+        except Exception as e:
+            log_info(f"Failed to get team by id from database: {e}")
+            handle_get_document_error(e)
+        return doc_to_core_team(team)
 
     def update_team(self, team: Team) -> Team:
-        # pylint: disable=no-member
-        team_document = TeamDocument.objects.get(id=team.id)  # type: ignore
-        team_document.save()
-        return _from_mongo_team(team_document)
+        team_doc = core_to_doc_team(team)
+        try:
+            team_saved = team_doc.save()
+        except Exception as e:
+            log_info(f"Failed to update team to database: {e}")
+            handle_save_document_error(e)
+        return doc_to_core_team(team_saved)
 
     def delete_team(self, team_id: str) -> None:
-        # pylint: disable=no-member
-        team = TeamDocument.objects.get(id=team_id)  # type: ignore
-        team.delete()
+        try:
+            # pylint: disable=no-member
+            team = TeamDocument.objects.get(id=team_id)  # type: ignore
+        except Exception as e:
+            log_info(f"Failed to get team by id to delete from database: {e}")
+            handle_get_document_error(e)
+        try:
+            team.delete()
+        except Exception as e:
+            log_info(f"Failed to delete team from database: {e}")
+            handle_delete_document_error(e)
 
 
 # Mappers
-def to_mongo_team(dataclass_obj: Team) -> TeamDocument:
-    # pylint: disable=no-member
-    team_members = (
-        UserDocument.objects.filter(id__in=dataclass_obj.team_members)  # type: ignore
-        if dataclass_obj.team_members
-        else []
-    )
-    team_leaders = (
-        UserDocument.objects.filter(id__in=dataclass_obj.team_leaders)  # type: ignore
-        if dataclass_obj.team_leaders
-        else []
-    )
-    return TeamDocument(
-        id=dataclass_obj.id,
-        team_members=team_members,
-        team_leaders=team_leaders,
-    )
+# core to document
+def core_to_doc_team(dataclass_obj: Team) -> TeamDocument:
+    try:
+        # pylint: disable=no-member
+        team_members = (
+            UserDocument.objects.filter(  # type: ignore
+                id__in=dataclass_obj.team_members
+            )
+            if dataclass_obj.team_members
+            else []
+        )
+    except Exception as e:
+        log_info(f"Failed to get users by ids from database: {e}")
+        handle_get_document_error(e)
+    try:
+        # pylint: disable=no-member
+        team_leaders = (
+            UserDocument.objects.filter(  # type: ignore
+                id__in=dataclass_obj.team_leaders
+            )
+            if dataclass_obj.team_leaders
+            else []
+        )
+    except Exception as e:
+        log_info(f"Failed to get users by ids from database: {e}")
+        handle_get_document_error(e)
+    try:
+        t_doc = TeamDocument(
+            id=dataclass_obj.id,
+            team_members=team_members,
+            team_leaders=team_leaders,
+        )
+    except Exception as e:
+        log_info(f"Failed to convert Team to TeamDocument: {e}")
+        handle_create_document_error(e)
+    return t_doc
 
 
-def _from_mongo_team(doc_obj: TeamDocument) -> Team:
-    return Team(
-        id=doc_obj.id,
-        team_members=[str(u.id) for u in doc_obj.team_members],
-        team_leaders=[str(u.id) for u in doc_obj.team_leaders],
-    )
+# document to core
+def doc_to_core_team(doc_obj: TeamDocument) -> Team:
+    try:
+        team = Team(
+            id=doc_obj.id,
+            team_members=[str(u.id) for u in doc_obj.team_members],
+            team_leaders=[str(u.id) for u in doc_obj.team_leaders],
+        )
+    except Exception as e:
+        log_info(f"Failed to convert TeamDocument to Team: {e}")
+        handle_create_core_object_error(e)
+    return team
