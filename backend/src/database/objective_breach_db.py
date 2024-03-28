eb924863e982567469593baa4aeea64935d45350
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-03-28 11:21:51 +0100

    Add error handling and logging in objective_breach_db.

--- backend/src/database/objective_breach_db.py
+++ backend/src/database/objective_breach_db.py
@@ -6,11 +6,19 @@ from bson import ObjectId
 from core.schedule import ObjectiveBreach, Schedule, Variable
 from database.db import DB
 from database.schedule_db import to_mongo_schedule
+from errors import (
+    handle_create_core_object_error,
+    handle_create_document_error,
+    handle_delete_document_error,
+    handle_get_document_error,
+    handle_save_document_error,
+)
 from models.objective_breach import ObjectiveBreach as ObjectiveBreachDocument
 from models.objective_breach import Variable as VariableDocument
 from models.schedule import Schedule as ScheduleDocument
 from models.shift import Shift as ShiftDocument
 from models.worker import Worker as WorkerDocument
+from services.logging import log_info
 
 
 class ObjectiveBreachDB:
@@ -20,126 +28,202 @@ class ObjectiveBreachDB:
     def create_objective_breach(
         self, objective_breach: ObjectiveBreach
     ) -> ObjectiveBreach:
-        ob_data = to_mongo_objective_breach(objective_breach)
-        ob_doc = ObjectiveBreachDocument(
-            id=str(ObjectId()),
-            objective_id=ob_data.objective_id,
-            objective_category=ob_data.objective_category,
-            variables=ob_data.variables,
-            hard_to_soft=ob_data.hard_to_soft,
-            description=ob_data.description,
-            schedule=ob_data.schedule,
-        )
-        ob_saved = ob_doc.save()
-        return _from_mongo_objective_breach(ob_saved)
+        ob_doc = core_to_doc_objective_breach(objective_breach)
+        ob_doc.id = str(ObjectId())
+        try:
+            ob_saved = ob_doc.save()
+        except Exception as e:
+            log_info(f"Failed to save objective breach to database: {e}")
+            handle_save_document_error(e)
+        return doc_to_core_objective_breach(ob_saved)
 
     def get_objective_breaches(
         self, schedules: List[Schedule]
     ) -> List[ObjectiveBreach]:
         s_docs = [to_mongo_schedule(s) for s in schedules]
-        # pylint: disable=no-member
-        objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
-            schedule__in=s_docs
-        )
-        return [_from_mongo_objective_breach(ob) for ob in list(objective_breaches)]
+        try:
+            # pylint: disable=no-member
+            objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
+                schedule__in=s_docs
+            )
+        except Exception as e:
+            log_info(f"Failed to get objective breaches from database: {e}")
+            handle_get_document_error(e)
+        return [doc_to_core_objective_breach(ob) for ob in list(objective_breaches)]
 
     def get_objective_breach_by_id(self, objective_breach_id: str) -> ObjectiveBreach:
-        # pylint: disable=no-member
-        objective_breach = ObjectiveBreachDocument.objects.get(  # type: ignore
-            id=objective_breach_id
-        )
-        return _from_mongo_objective_breach(objective_breach)
+        try:
+            # pylint: disable=no-member
+            objective_breach = ObjectiveBreachDocument.objects.get(  # type: ignore
+                id=objective_breach_id
+            )
+        except Exception as e:
+            log_info(f"Failed to get objective breach by id from database: {e}")
+            handle_get_document_error(e)
+        return doc_to_core_objective_breach(objective_breach)
 
     def get_objective_breaches_by_schedule_id(
         self, schedule_id: str
     ) -> List[ObjectiveBreach]:
-        # pylint: disable=no-member
-        objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
-            schedule=schedule_id
-        )
-        return [_from_mongo_objective_breach(ob) for ob in list(objective_breaches)]
+        try:
+            # pylint: disable=no-member
+            objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
+                schedule=schedule_id
+            )
+        except Exception as e:
+            log_info(
+                f"Failed to get objective breaches by schedule id from database: {e}"
+            )
+            handle_get_document_error(e)
+        return [doc_to_core_objective_breach(ob) for ob in list(objective_breaches)]
 
     def get_objective_breaches_by_worker_id(
         self, worker_id: str
     ) -> List[ObjectiveBreach]:
-        # pylint: disable=no-member
-        objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
-            variables__worker=worker_id
-        )
-        return [_from_mongo_objective_breach(ob) for ob in list(objective_breaches)]
+        try:
+            # pylint: disable=no-member
+            objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
+                variables__worker=worker_id
+            )
+        except Exception as e:
+            log_info(
+                f"Failed to get objective breaches by worker id from database: {e}"
+            )
+            handle_get_document_error(e)
+        return [doc_to_core_objective_breach(ob) for ob in list(objective_breaches)]
 
     def update_objective_breach(
         self, objective_breach: ObjectiveBreach
     ) -> ObjectiveBreach:
-        document = to_mongo_objective_breach(objective_breach)
-        document_saved = document.save()
-        return _from_mongo_objective_breach(document_saved)
+        document = core_to_doc_objective_breach(objective_breach)
+        try:
+            document_saved = document.save()
+        except Exception as e:
+            log_info(f"Failed to update objective breach: {e}")
+            handle_save_document_error(e)
+        return doc_to_core_objective_breach(document_saved)
 
     def delete_objective_breach(self, objective_breach_id: str) -> None:
-        # pylint: disable=no-member
-        objective_breach = ObjectiveBreachDocument.objects.get(  # type: ignore
-            id=objective_breach_id
-        )
-        objective_breach.delete()
+        try:
+            # pylint: disable=no-member
+            objective_breach = ObjectiveBreachDocument.objects.get(  # type: ignore
+                id=objective_breach_id
+            )
+        except Exception as e:
+            log_info(f"Failed to get objective breach by id to delete: {e}")
+            handle_get_document_error(e)
+        try:
+            objective_breach.delete()
+        except Exception as e:
+            log_info(f"Failed to delete objective breach: {e}")
+            handle_delete_document_error(e)
 
     def delete_objective_breaches_by_schedule_id(self, schedule_id: str) -> None:
-        # pylint: disable=no-member
-        ObjectiveBreachDocument.objects.filter(  # type: ignore
-            schedule=schedule_id
-        ).delete()
+        try:
+            # pylint: disable=no-member
+            objective_breaches = ObjectiveBreachDocument.objects.filter(  # type: ignore
+                schedule=schedule_id
+            )
+        except Exception as e:
+            log_info(f"Failed to get objective breaches by schedule id to delete: {e}")
+            handle_get_document_error(e)
+        try:
+            for ob in objective_breaches:
+                ob.delete()
+        except Exception as e:
+            log_info(f"Failed to delete objective breaches: {e}")
+            handle_delete_document_error(e)
 
 
 # Mappers
-def to_mongo_variable(dataclass_obj: Variable) -> VariableDocument:
-    # pylint: disable=no-member, R0801
-    worker = WorkerDocument.objects.get(id=dataclass_obj.worker_id)  # type: ignore
-    shift = ShiftDocument.objects.get(id=dataclass_obj.shift_id)  # type: ignore
-    return VariableDocument(
-        worker=worker,
-        date=datetime(
-            dataclass_obj.date.year,
-            dataclass_obj.date.month,
-            dataclass_obj.date.day,
-        ),
-        shift=shift,
-    )
+# core to document
+def core_to_doc_variable(dataclass_obj: Variable) -> VariableDocument:
+    try:
+        # pylint: disable=no-member, R0801
+        worker = WorkerDocument.objects.get(id=dataclass_obj.worker_id)  # type: ignore
+    except Exception as e:
+        log_info(f"Failed to get worker by id: {e}")
+        handle_get_document_error(e)
+    # pylint: disable=R0801
+    try:
+        # pylint: disable=no-member
+        shift = ShiftDocument.objects.get(id=dataclass_obj.shift_id)  # type: ignore
+    except Exception as e:
+        log_info(f"Failed to get shift by id: {e}")
+        handle_get_document_error(e)
+    # pylint: disable=R0801
+    try:
+        v_doc = VariableDocument(
+            worker=worker,
+            date=datetime(
+                dataclass_obj.date.year,
+                dataclass_obj.date.month,
+                dataclass_obj.date.day,
+            ),
+            shift=shift,
+        )
+    except Exception as e:
+        log_info(f"Failed to convert Variable to VariableDocument: {e}")
+        handle_create_document_error(e)
+    return v_doc
 
 
-def to_mongo_objective_breach(
+def core_to_doc_objective_breach(
     dataclass_obj: ObjectiveBreach,
 ) -> ObjectiveBreachDocument:
-    # pylint: disable=no-member
-    schedule = ScheduleDocument.objects.get(  # type: ignore
-        id=dataclass_obj.schedule_id
-    )
-    return ObjectiveBreachDocument(
-        id=dataclass_obj.id,
-        objective_id=dataclass_obj.objective_id,
-        objective_category=dataclass_obj.objective_category,
-        variables=[to_mongo_variable(v) for v in dataclass_obj.variables],
-        hard_to_soft=dataclass_obj.hard_to_soft,
-        description=dataclass_obj.description,
-        schedule=schedule,
-    )
+    # pylint: disable=R0801
+    try:
+        # pylint: disable=no-member
+        schedule = ScheduleDocument.objects.get(  # type: ignore
+            id=dataclass_obj.schedule_id
+        )
+    except Exception as e:
+        log_info(f"Failed to get schedule by id: {e}")
+        handle_get_document_error(e)
+    try:
+        ob_doc = ObjectiveBreachDocument(
+            id=dataclass_obj.id,
+            objective_id=dataclass_obj.objective_id,
+            objective_category=dataclass_obj.objective_category,
+            variables=[core_to_doc_variable(v) for v in dataclass_obj.variables],
+            hard_to_soft=dataclass_obj.hard_to_soft,
+            description=dataclass_obj.description,
+            schedule=schedule,
+        )
+    except Exception as e:
+        log_info(f"Failed to convert ObjectiveBreach to ObjectiveBreachDocument: {e}")
+        handle_create_document_error(e)
+    return ob_doc
 
 
-def _from_mongo_variable(doc_var: VariableDocument) -> Variable:
-    return Variable(
-        worker_id=doc_var.worker.id,
-        date=doc_var.date.date(),
-        shift_id=doc_var.shift.id,
-    )
+# document to core
+def doc_to_core_mongo_variable(doc_var: VariableDocument) -> Variable:
+    try:
+        variable = Variable(
+            worker_id=doc_var.worker.id,
+            date=doc_var.date.date(),
+            shift_id=doc_var.shift.id,
+        )
+    except Exception as e:
+        log_info(f"Failed to convert VariableDocument to Variable: {e}")
+        handle_create_core_object_error(e)
+    return variable
 
 
-def _from_mongo_objective_breach(
+def doc_to_core_objective_breach(
     doc_obj: ObjectiveBreachDocument,
 ) -> ObjectiveBreach:
-    return ObjectiveBreach(
-        id=doc_obj.id,
-        objective_id=doc_obj.objective_id,
-        objective_category=doc_obj.objective_category,
-        variables=[_from_mongo_variable(v) for v in doc_obj.variables],
-        hard_to_soft=doc_obj.hard_to_soft,
-        description=doc_obj.description,
-        schedule_id=doc_obj.schedule.id,
-    )
+    try:
+        objective_breach = ObjectiveBreach(
+            id=doc_obj.id,
+            objective_id=doc_obj.objective_id,
+            objective_category=doc_obj.objective_category,
+            variables=[doc_to_core_mongo_variable(v) for v in doc_obj.variables],
+            hard_to_soft=doc_obj.hard_to_soft,
+            description=doc_obj.description,
+            schedule_id=doc_obj.schedule.id,
+        )
+    except Exception as e:
+        log_info(f"Failed to convert ObjectiveBreachDocument to ObjectiveBreach: {e}")
+        handle_create_core_object_error(e)
+    return objective_breach
