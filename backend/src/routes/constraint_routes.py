fe41ac31c679c722b706231cbbf5dd01d0874798
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-04-01 13:41:58 +0200

    Add error handling and logging to constraint routes.

--- backend/src/routes/constraint_routes.py
+++ backend/src/routes/constraint_routes.py
@@ -2,12 +2,20 @@ from dataclasses import asdict
 from typing import List
 
 import humps
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import TypeAdapter
 
 from core.constraint import Block, ConstraintBuild, MissingProperty
+from errors import (
+    MessageTypeError,
+    NotAuthorizedError,
+    handle_create_core_object_error,
+    handle_message_errors,
+    handle_routes_errors,
+)
 from integrations.authentication import SessionContainerType, authn_verify_session
 from integrations.authorization import authz_check
+from logger import log_info
 from routes.api_model import (
     BlockMessage,
     ConstraintBuildMessage,
@@ -28,17 +36,21 @@ async def create_constraint(
     req: ConstraintBuildMessage,
     session: SessionContainerType = Depends(authn_verify_session()),
 ) -> ConstraintBuildMessage:
-    if not await authz_check(
-        session.get_user_id(), "create-constraint", "team", team_id
-    ):
-        raise HTTPException(
-            status_code=403,
-            detail="You do not have permission to create a constraint",
-        )
-    cb_data = api_msg_to_constraint_build(req)
-    cb_data.text = blocks_to_string(cb_data.blocks)
-    constraint_build = constraint_build_db.create_constraint_build(cb_data)
-    return constraint_build_to_api_msg(constraint_build)
+    try:
+        if not await authz_check(
+            session.get_user_id(), "create-constraint", "team", team_id
+        ):
+            raise NotAuthorizedError(
+                "You do not have permission to create a constraint"
+            )
+        cb_data = msg_to_core_constraint_build(req)
+        cb_data.text = blocks_to_string(cb_data.blocks)
+        constraint_build = constraint_build_db.create_constraint_build(cb_data)
+        response = core_to_msg_constraint_build(constraint_build)
+    except Exception as e:
+        log_info("Failed to create constraint")
+        handle_routes_errors(e)
+    return response
 
 
 @router.get("/constraints/teams/{team_id}")
@@ -46,38 +58,40 @@ async def get_constraints(
     team_id: str,
     session: SessionContainerType = Depends(authn_verify_session()),
 ) -> List[ConstraintBuildMessage]:
-    if not await authz_check(
-        session.get_user_id(), "read-constraints", "team", team_id
-    ):
-        raise HTTPException(
-            status_code=403,
-            detail="You do not have permission to get constraints",
-        )
-    constraint_builds = constraint_build_db.get_constraint_builds(team_id)
-    return [constraint_build_to_api_msg(cb) for cb in constraint_builds]
+    try:
+        if not await authz_check(
+            session.get_user_id(), "read-constraints", "team", team_id
+        ):
+            raise NotAuthorizedError("You do not have permission to get constraints")
+        constraint_builds = constraint_build_db.get_constraint_builds(team_id)
+        response = [core_to_msg_constraint_build(cb) for cb in constraint_builds]
+    except Exception as e:
+        log_info("Failed to get constraints")
+        handle_routes_errors(e)
+    return response
 
 
 @router.put("/constraints/{constraint_build_id}/teams/{team_id}")
 async def update_constraint(
-    constraint_build_id: str,
     team_id: str,
     updated_constraint_build: ConstraintBuildMessage,
     session: SessionContainerType = Depends(authn_verify_session()),
 ) -> ConstraintBuildMessage:
-    if not await authz_check(
-        session.get_user_id(), "update-constraint", "team", team_id
-    ):
-        raise HTTPException(
-            status_code=403,
-            detail="You do not have permission to update a constraint",
-        )
-    cb_data = api_msg_to_constraint_build(updated_constraint_build)
-    existing_cb = constraint_build_db.get_constraint_build_by_id(constraint_build_id)
-    if not existing_cb:
-        raise HTTPException(status_code=404, detail="Constraint does not exist")
-    cb_data.text = blocks_to_string(cb_data.blocks)
-    cb_updated = constraint_build_db.update_constraint_build(cb_data)
-    return constraint_build_to_api_msg(cb_updated)
+    try:
+        if not await authz_check(
+            session.get_user_id(), "update-constraint", "team", team_id
+        ):
+            raise NotAuthorizedError(
+                "You do not have permission to update a constraint"
+            )
+        cb_data = msg_to_core_constraint_build(updated_constraint_build)
+        cb_data.text = blocks_to_string(cb_data.blocks)
+        cb_updated = constraint_build_db.update_constraint_build(cb_data)
+        response = core_to_msg_constraint_build(cb_updated)
+    except Exception as e:
+        log_info("Failed to update constraint")
+        handle_routes_errors(e)
+    return response
 
 
 @router.delete("/constraints/{constraint_build_id}/teams/{team_id}")
@@ -86,56 +100,99 @@ async def delete_constraint(
     team_id: str,
     session: SessionContainerType = Depends(authn_verify_session()),
 ):
-    if not await authz_check(
-        session.get_user_id(), "delete-constraint", "team", team_id
-    ):
-        raise HTTPException(
-            status_code=403,
-            detail="You do not have permission to delete a constraint",
-        )
-    delete_constraint_build_and_dependencies(constraint_build_id)
+    try:
+        if not await authz_check(
+            session.get_user_id(), "delete-constraint", "team", team_id
+        ):
+            raise NotAuthorizedError(
+                "You do not have permission to delete a constraint"
+            )
+        delete_constraint_build_and_dependencies(constraint_build_id)
+    except Exception as e:
+        log_info("Failed to delete constraint")
+        handle_routes_errors(e)
     return {"message": "Constraint deleted"}
 
 
-def block_to_api_msg(block: Block) -> BlockMessage:
-    data = asdict(block)
+# Mappers
+# core to message
+def core_to_msg_block(block: Block) -> BlockMessage:
+    try:
+        data = asdict(block)
+    except Exception as e:
+        log_info("Failed to convert core Block to dictionary")
+        raise MessageTypeError(str(e)) from e
     as_dict = humps.camelize(data)
     validator = TypeAdapter(BlockMessage)
-    return validator.validate_python(as_dict)
+    try:
+        b_msg = validator.validate_python(as_dict)
+    except Exception as e:
+        log_info("Failed to convert core Block to BlockMessage")
+        handle_message_errors(e)
+    return b_msg
 
 
-def missing_property_to_api_msg(
+def core_to_msg_missing_property(
     missing_property: MissingProperty,
 ) -> MissingPropertyMessage:
-    data = asdict(missing_property)
+    try:
+        data = asdict(missing_property)
+    except Exception as e:
+        log_info("Failed to convert core MissingProperty to dictionary")
+        raise MessageTypeError(str(e)) from e
     as_dict = humps.camelize(data)
     validator = TypeAdapter(MissingPropertyMessage)
-    return validator.validate_python(as_dict)
+    try:
+        mp_msg = validator.validate_python(as_dict)
+    except Exception as e:
+        log_info("Failed to convert core MissingProperty to MissingPropertyMessage")
+        handle_message_errors(e)
+    return mp_msg
 
 
-def constraint_build_to_api_msg(
+def core_to_msg_constraint_build(
     constraint_build: ConstraintBuild,
 ) -> ConstraintBuildMessage:
-    blocks = [block_to_api_msg(b) for b in constraint_build.blocks]
+    blocks = [core_to_msg_block(b) for b in constraint_build.blocks]
     missing_properties = [
-        missing_property_to_api_msg(mp) for mp in constraint_build.missing_properties
+        core_to_msg_missing_property(mp) for mp in constraint_build.missing_properties
     ]
-    data = asdict(constraint_build)
+    try:
+        data = asdict(constraint_build)
+    except Exception as e:
+        log_info("Failed to convert core ConstraintBuild to dictionary")
+        raise MessageTypeError(str(e)) from e
     data["blocks"] = blocks
     data["missing_properties"] = missing_properties
     as_dict = humps.camelize(data)
     validator = TypeAdapter(ConstraintBuildMessage)
-    return validator.validate_python(as_dict)
+    try:
+        cb_msg = validator.validate_python(as_dict)
+    except Exception as e:
+        log_info("Failed to convert core ConstraintBuild to ConstraintBuildMessage")
+        handle_message_errors(e)
+    return cb_msg
 
 
-def api_msg_to_block(msg: BlockMessage) -> Block:
+# message to core
+def msg_to_core_block(msg: BlockMessage) -> Block:
     data_snake = humps.decamelize(msg.model_dump())
-    return Block(**data_snake)
+    try:
+        block = Block(**data_snake)
+    except Exception as e:
+        log_info("Failed to convert BlockMessage to Block")
+        handle_create_core_object_error(e)
+    return block
 
 
-def api_msg_to_constraint_build(
+def msg_to_core_constraint_build(
     msg: ConstraintBuildMessage,
 ) -> ConstraintBuild:
     data_snake = humps.decamelize(msg.model_dump())
-    data_snake["blocks"] = [api_msg_to_block(b) for b in msg.blocks]
-    return ConstraintBuild(**data_snake)
+    data_snake["blocks"] = [msg_to_core_block(b) for b in msg.blocks]
+    try:
+        constraint_build = ConstraintBuild(**data_snake)
+    except Exception as e:
+        log_info("Failed to convert ConstraintBuildMessage to ConstraintBuild")
+        handle_create_core_object_error(e)
+    return constraint_build
