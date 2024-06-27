9d827c05ba2009a2ba83c6df926183d463f6aec7
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-06-27 12:53:16 +0200

    Add account menu button to nav app bar.

--- new_frontend/src/components/app-bar/account-menu.tsx
+++ new_frontend/src/components/app-bar/account-menu.tsx
@@ -1,25 +1,28 @@
+"use client";
+
 import * as React from "react";
 import SessionReact from "supertokens-auth-react/recipe/session";
 import SuperTokensReact from "supertokens-auth-react";
 import { useTranslation } from "react-i18next";
+import Link from "next/link";
 // MUI
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-// Stores
-// import { useTeamStore } from "../../stores/teamStore";
 
-interface Props {
-  tabs: { id: string; label: string; type: string }[];
-  handleSelectTab: (tabId: string) => void;
-}
-
-export default function AccountMenu({ tabs, handleSelectTab }: Props) {
+export default function AccountMenu({ lng }: { lng: string }) {
   const { t } = useTranslation();
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  // const clearTeams = useTeamStore((state) => state.clearTeams);
+
+  const links: { name: string; label: string; href: string }[] = [
+    {
+      name: "profile",
+      label: t("common.profile"),
+      href: `/${lng}/plan/profile`,
+    },
+  ];
 
   const handleLogout = async () => {
     await SessionReact.signOut();
@@ -62,19 +65,21 @@ export default function AccountMenu({ tabs, handleSelectTab }: Props) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        {tabs
-          .filter((t) => t.type === "config")
-          .map((tab) => (
+        {links.map((link) => {
+          return (
             <MenuItem
-              key={tab.id}
-              onClick={() => {
-                handleSelectTab(tab.id);
-                handleClose();
-              }}
+              key={link.name}
+              // onClick={() => {
+              //   handleSelectTab(tab.id);
+              //   handleClose();
+              // }}
             >
-              {tab.label}
+              <Link key={link.name} href={link.href}>
+                {link.label}
+              </Link>
             </MenuItem>
-          ))}
+          );
+        })}
         {/* <MenuItem onClick={handleClose}>My account</MenuItem> */}
         <MenuItem onClick={handleLogout}>{t("common.sign_out")}</MenuItem>
       </Menu>
--- new_frontend/src/components/app-bar/nav-links.tsx
+++ new_frontend/src/components/app-bar/nav-links.tsx
@@ -6,20 +6,6 @@ import { useTranslation } from "@/app/i18n/client";
 // MUI
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-// Components
-// import TabButton from "@/app/components/app-bar/tab-button";
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-// const links = [
-//   { name: "Home", href: "/dashboard", icon: HomeIcon },
-//   {
-//     name: "Invoices",
-//     href: "/dashboard/invoices",
-//     icon: DocumentDuplicateIcon,
-//   },
-//   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
-// ];
 
 export default function NavLinks({ lng }: { lng: string }) {
   const { t } = useTranslation(lng, "app-bar");
