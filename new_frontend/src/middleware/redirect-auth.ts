bf7ccb5079df698ed2fbdae945bf1040b3fedefc
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-07-10 19:46:54 +0200

    Change the redirectAuth middleware to redirect to the exact same path, just with the language code at the start.

--- new_frontend/src/middleware/redirect-auth.ts
+++ new_frontend/src/middleware/redirect-auth.ts
@@ -2,30 +2,57 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function redirectAuth(req: NextRequest) {
   const url = req.nextUrl.clone();
-  const i18nextCookie = req.cookies.get("i18next");
-  const acceptLanguage = req.headers.get("accept-language");
 
-  if (url.pathname === "/auth") {
+  if (url.pathname.startsWith("/auth")) {
+    const urlPathnameOriginal = url.pathname;
+    const i18nextCookie = req.cookies.get("i18next");
+    const acceptLanguage = req.headers.get("accept-language");
+
     if (i18nextCookie) {
       if (i18nextCookie.value === "fr") {
-        url.pathname = "/fr/auth";
+        url.pathname = `/fr${urlPathnameOriginal}`;
       } else if (i18nextCookie.value === "en") {
-        url.pathname = "/en/auth";
+        url.pathname = `/en${urlPathnameOriginal}`;
       } else if (i18nextCookie.value === "es") {
-        url.pathname = "/es/auth";
+        url.pathname = `/es${urlPathnameOriginal}`;
       }
     } else if (acceptLanguage) {
       if (acceptLanguage.startsWith("fr")) {
-        url.pathname = "/fr/auth";
+        url.pathname = `/fr${urlPathnameOriginal}`;
       } else if (acceptLanguage.startsWith("en")) {
-        url.pathname = "/en/auth";
+        url.pathname = `/en${urlPathnameOriginal}`;
       } else if (acceptLanguage.startsWith("es")) {
-        url.pathname = "/es/auth";
+        url.pathname = `/es${urlPathnameOriginal}`;
       }
     } else {
-      url.pathname = "/en/auth";
+      url.pathname = `/en${urlPathnameOriginal}`;
     }
 
     return NextResponse.redirect(url);
   }
 }
+
+//   if (url.pathname === "/auth") {
+//     if (i18nextCookie) {
+//       if (i18nextCookie.value === "fr") {
+//         url.pathname = "/fr/auth";
+//       } else if (i18nextCookie.value === "en") {
+//         url.pathname = "/en/auth";
+//       } else if (i18nextCookie.value === "es") {
+//         url.pathname = "/es/auth";
+//       }
+//     } else if (acceptLanguage) {
+//       if (acceptLanguage.startsWith("fr")) {
+//         url.pathname = "/fr/auth";
+//       } else if (acceptLanguage.startsWith("en")) {
+//         url.pathname = "/en/auth";
+//       } else if (acceptLanguage.startsWith("es")) {
+//         url.pathname = "/es/auth";
+//       }
+//     } else {
+//       url.pathname = "/en/auth";
+//     }
+
+//     return NextResponse.redirect(url);
+//   }
+// }
