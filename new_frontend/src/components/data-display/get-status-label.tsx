043579adadea8b719ff93d0832778d303aec996d
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-08-11 19:40:50 +0200

    Convert GetStatusLabel React component to useStatusLabel custom hook for usage in ScheduleSelector.

--- new_frontend/src/components/data-display/get-status-label.tsx
+++ new_frontend/src/components/data-display/get-status-label.tsx
@@ -11,3 +11,23 @@ export const GetStatusLabel = (lng: string, status: string) => {
   ];
   return statusOptions.find((option) => option.name === status)?.label || "";
 };
+
+// Custom hook version
+const useStatusLabel = (lng: string) => {
+  const { t } = useTranslation(lng, "campaign-page");
+
+  const getStatusLabel = (status: string): string => {
+    const statusOptions: Record<string, string>[] = [
+      { name: "Not solved", label: t("not_solved") },
+      { name: "Solved", label: t("solved") },
+      { name: "No solution", label: t("no_solution") },
+      { name: "Soft breached", label: t("soft_breach") },
+      { name: "Hard breached", label: t("hard_breach") },
+    ];
+    return statusOptions.find((option) => option.name === status)?.label || "";
+  };
+
+  return getStatusLabel;
+};
+
+export default useStatusLabel;
