48535e28ff7f890226bdad783c75c97ad5740534
FelipeKha <62256236+FelipeKha@users.noreply.github.com>
2024-09-09 19:44:58 +0300

    Delete old frontend, and rename new_frontend to frontend. (#383)
    Co-authored-by: Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>

--- frontend/src/utils/cookie.ts
+++
@@ -1,9 +0,0 @@
-export function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    const cookieValue = parts.pop()?.split(";").shift();
-    return cookieValue !== undefined ? cookieValue : "";
-  }
-  return "";
-}
