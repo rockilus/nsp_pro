8ab66ae2758659b0b416545ca97d36192e56e45d
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-09-10 18:00:46 +0200

    Add loading skeletons to schedule tab.

--- frontend/src/app/[lng]/plan/schedule/loading.tsx
+++ frontend/src/app/[lng]/plan/schedule/loading.tsx
@@ -1,5 +1,12 @@
+// Skeletons
 import ScheduleSkeleton from "../../../../components/skeletons/schedule-skeleton";
+// Styles
+import "../../../../styles/tab-container-styles.css";
 
 export default function Loading() {
-  return <ScheduleSkeleton />;
+  return (
+    <div className="tab-container-ultrawide">
+      <ScheduleSkeleton />
+    </div>
+  );
 }
