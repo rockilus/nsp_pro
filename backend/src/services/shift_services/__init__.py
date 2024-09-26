b0d9c342a21fec8459d4045d88180b608b646900
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2024-09-26 14:05:55 +0200

    Add default leave shifts when creating new team upon sign up.

--- backend/src/services/shift_services/__init__.py
+++ backend/src/services/shift_services/__init__.py
@@ -1,3 +1,4 @@
+from services.shift_services.create_shift import create_default_leave_shifts
 from services.shift_services.delete_shift import delete_shift
 from services.shift_services.delete_shift_dimension import delete_shift_dimension
 from services.shift_services.update_shift_property import (
@@ -5,6 +6,7 @@ from services.shift_services.update_shift_property import (
 )
 
 __all__ = [
+    "create_default_leave_shifts",
     "delete_shift",
     "delete_shift_dimension",
     "create_or_update_shift_property",
