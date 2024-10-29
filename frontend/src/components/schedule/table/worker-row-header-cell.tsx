bed6da250b6017596007ac97fe14e103c2bc54c1
FelipeKha <62256236+FelipeKha@users.noreply.github.com>
2024-10-29 15:13:11 +0200

    Reformat worker row header cell (#498)
    * Reformat worker row header cell.
    
    * Add translation for worker row header.
    
    ---------
    
    Co-authored-by: Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>

---
+++ frontend/src/components/schedule/table/worker-row-header-cell.tsx
@@ -0,0 +1,125 @@
+import dayjs from "dayjs";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import { useTranslation } from "../../../app/i18n/client";
+// MUI
+import TableCell from "@mui/material/TableCell";
+// Styles
+import "./worker-row-header-cell.css";
+// Types
+import { ShiftT, ShiftType } from "../../../types/shift";
+import { WorkerT } from "../../../types/worker";
+import { AssignmentT, ScheduleT } from "../../../types/schedule";
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+export default function WorkerRowHeaderCell({
+  lng,
+  shifts,
+  worker,
+  assignments,
+  schedule,
+}: {
+  lng: string;
+  shifts: ShiftT[];
+  worker: WorkerT;
+  assignments: AssignmentT[];
+  schedule: ScheduleT;
+}) {
+  const { t } = useTranslation(lng, "schedule-page");
+
+  const assignmentsWorker = assignments.filter(
+    (assignment) =>
+      assignment.workerId === worker.id &&
+      assignment.date.isSameOrAfter(schedule.startDate, "day") &&
+      assignment.date.isSameOrBefore(schedule.endDate, "day")
+  );
+
+  const shiftMap: { [key: string]: ShiftT } = shifts.reduce((map, shift) => {
+    map[shift.id] = shift;
+    return map;
+  }, {} as { [key: string]: ShiftT });
+
+  const calcWeeklyWorkTimeActual = () => {
+    const workerTotalWorkTimeActual = assignmentsWorker.reduce(
+      (acc, assignment) => {
+        const shift = shiftMap[assignment.shiftId];
+        if (shift) {
+          const duration = shift.endTime.diff(shift.startTime, "hour", true);
+          return acc + duration;
+        }
+        return acc;
+      },
+      0
+    );
+    const numWeeksSchedule =
+      (schedule.endDate.diff(schedule.startDate, "day") + 1) / 7;
+    return workerTotalWorkTimeActual / numWeeksSchedule;
+  };
+
+  const calcDutiesPerMonthActual = () => {
+    const workerTotalDutiesActual = assignmentsWorker.reduce(
+      (acc, assignment) => {
+        const shift = shiftMap[assignment.shiftId];
+        if (shift && shift.shiftType === ShiftType.DUTY) {
+          return acc + 1;
+        }
+        return acc;
+      },
+      0
+    );
+    const numMonthsSchedule = schedule.endDate.diff(
+      schedule.startDate,
+      "month",
+      true
+    );
+    return workerTotalDutiesActual / numMonthsSchedule;
+  };
+
+  const workerWeeklyWorkTimeActual = calcWeeklyWorkTimeActual();
+  const workerDutiesPerMonthActual = calcDutiesPerMonthActual();
+
+  return (
+    <TableCell
+      sx={{
+        position: "sticky",
+        left: 0,
+        backgroundColor: "#FFFFFF",
+        padding: 0,
+      }}
+    >
+      <div className="worker-row-header-cell-container">
+        <span className="worker-name">{worker.name}</span>
+        <div className="worker-stats-item">
+          <div
+            className={`worker-stats-container ${
+              workerWeeklyWorkTimeActual > worker.weeklyHoursDesired && "breach"
+            }`}
+          >
+            <span className="worker-stats">
+              {workerWeeklyWorkTimeActual.toFixed(1)}
+            </span>
+            <span className="worker-stats-slash">/</span>
+            <span className="worker-stats">{worker.weeklyHoursDesired}</span>
+          </div>
+          <span className="worker-stats-label">{t("h/week")}</span>
+        </div>
+        <div className="worker-stats-item">
+          <div
+            className={`worker-stats-container ${
+              workerDutiesPerMonthActual > worker.dutiesPerMonth && "breach"
+            }`}
+          >
+            <span className="worker-stats">
+              {workerDutiesPerMonthActual.toFixed(1)}
+            </span>
+            <span className="worker-stats-slash">/</span>
+            <span className="worker-stats">{worker.dutiesPerMonth}</span>
+          </div>
+          <span className="worker-stats-label">{t("duties/month")}</span>
+        </div>
+      </div>
+    </TableCell>
+  );
+}
