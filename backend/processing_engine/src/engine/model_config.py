fd955c465a5ed27d2d069dbcfde8008ea330f8a4
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-01-27 14:22:46 +0100

    Calibrate penalties for engine's model.

--- backend/processing_engine/src/engine/model_config.py
+++ backend/processing_engine/src/engine/model_config.py
@@ -12,28 +12,28 @@ from engine.types import (
 model_config = ModelConfig(
     penalties=Penalties(
         system_constraint=SystemConstraintPenalty(
-            coverage=CoveragePenalty(duty=10, normal=1),
-            duty_recup=100,
-            worker_shift_filter=100,
-            link_shift=100,
-            weekly_worktime_max=100,
-            weekly_worktime_desired=100,
-            weekly_worktime_contract=100,
-            monthly_duties_max=100,
-            monthly_duties_desired=100,
+            coverage=CoveragePenalty(duty=1000, normal=200),
+            duty_recup=10000,
+            worker_shift_filter=10000,
+            link_shift=5,
+            weekly_worktime_max=5,
+            weekly_worktime_desired=3,
+            weekly_worktime_contract=1,
+            monthly_duties_max=5,
+            monthly_duties_desired=3,
         ),
         user_constraint=UserConstraintPenalty(
-            eve=Penalty(hard=1, soft=1),
-            fai=Penalty(hard=1, soft=1),
-            fil=Penalty(hard=100, soft=1),
-            ord=Penalty(hard=1, soft=1),
-            seq=Penalty(hard=1, soft=1),
-            sum=Penalty(hard=100, soft=1),
-            request=Penalty(hard=10, soft=1),
+            eve=Penalty(hard=100, soft=10),
+            fai=Penalty(hard=100, soft=10),
+            fil=Penalty(hard=100, soft=10),
+            ord=Penalty(hard=100, soft=10),
+            seq=Penalty(hard=100, soft=10),
+            sum=Penalty(hard=100, soft=10),
+            request=Penalty(hard=100, soft=10),
         ),
     ),
     solver_params=SolverParams(
-        max_time_in_seconds=20, solve_strategy=SolveStrategy.SEQUENTIAL
+        max_time_in_seconds=20, solve_strategy=SolveStrategy.HARD_TO_SOFT
     ),
 )
 
@@ -49,19 +49,19 @@ HARD CONSTRAINTS:
 - Duty recuperation: penalty
 
 SOFT CONSTRAINTS:
-- Duty coverage: penalty * delta
-- Non duty coverage: penalty * delta
-- Requests: penalty (change to penalty per request)
+- Duty coverage: penalty * delta - 1
+- Non duty coverage: penalty * delta - 0.2
+- Requests: penalty (change to penalty per request) - 0.1 / 0.01
 - Custom constraints
-    - Sum: penalty * delta
+    - Sum: penalty * delta - 0.1 / 0.01
     - Seq: penalty * delta
     - Ord: penalty
     - Fil: penalty * num breaches
     - Eve
     - Fai
-- Linked shifts: penalty
-- Weekly worktime max: penalty * delta
-- Monthly number of duties max: penalty * delta
+- Linked shifts: penalty - 0.005
+- Weekly worktime max: penalty * delta - 0.005
+- Monthly number of duties max: penalty * delta - 0.005
 - Weekly worktime desired: penalty * delta
 - Monthly number of duties desired: penalty * delta
 - Weekly worktime contract: penalty * delta
