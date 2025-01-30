846255ad06d487f6a7e3448a8c65590aa24ccb38
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-01-30 16:22:42 +0100

    Change add request method to impose penalty if any of the variables in the request is not respected, instead of one penalty per variable (e.g. asking for one week holiday, same penalty applied if the model gives 6 days of holiday or 1, all in all the request is not respected). This also results in one breach being created, instead of one per day.

--- backend/processing_engine/src/engine/model/add_request.py
+++ backend/processing_engine/src/engine/model/add_request.py
@@ -25,30 +25,78 @@ class AddRequest:
             c_variables: List[cp_model.IntVar] = [
                 self.variables[a] for a in r.assignments
             ]
-            for var in c_variables:
-                if r.hard and not hard_to_soft:
-                    self.model.Add(var == 0 if r.negative else var == 1)
-                    continue
+            c_var_len = len(c_variables)
+
+            if r.hard and not hard_to_soft:
+                self.model.Add(
+                    sum(c_variables) == 0
+                    if r.negative
+                    else sum(c_variables) == c_var_len
+                )
+            else:
                 penalty = (
                     self.model_config.penalties.user_constraint.request.hard
                     if r.hard
                     else self.model_config.penalties.user_constraint.request.soft
                 )
-                cstr_vars: List[cp_model.IntVar | cp_model._NotBooleanVariable] = [var]
-                if any(
-                    # pylint: disable=protected-access
-                    isinstance(var, cp_model._NotBooleanVariable)
-                    for var in cstr_vars
-                ):
-                    raise ValueError("Request constraints should be boolean variables")
                 var_name = build_var_name_constraint(
-                    r, cstr_vars, ObjectiveCategory.REQUEST  # type: ignore
+                    r, c_variables, ObjectiveCategory.REQUEST
                 )
-                # pylint: disable=R0801
+                # var_request_ok = self.model.NewBoolVar(var_name)
+                # self.model.Add(
+                #     var_request_ok == (sum(c_variables) == 0)
+                #     if r.negative
+                #     else var_request_ok == (sum(c_variables) == c_var_len)
+                # )
+
                 lit = self.model.NewBoolVar(var_name)
+                # cstr_vars = [var_request_ok, lit]
+                # self.model.AddBoolOr(cstr_vars)
                 if r.negative:
-                    cstr_vars = [var.Not() for var in cstr_vars]
-                cstr_vars.append(lit)
-                self.model.AddBoolOr(cstr_vars)
+                    # (x or y) => p
+                    # model.AddImplication(x, p)
+                    # model.AddImplication(y, p)
+                    for var in c_variables:
+                        self.model.AddImplication(var, lit)
+                else:
+                    for var in c_variables:
+                        self.model.AddImplication(var.Not(), lit)
+                    # a and b and c => d
+                    # model.Add(d == 1).OnlyEnforceIf([a, b, c])
+                    # self.model.Add(lit == 1).OnlyEnforceIf(
+                    #     [c.Not() for c in c_variables]
+                    # )
                 self.obj.bool_vars.append(lit)
                 self.obj.bool_coeffs.append(penalty)
+
+            # for var in c_variables:
+            #     if r.hard and not hard_to_soft:
+            #         self.model.Add(var == 0 if r.negative else var == 1)
+            #         continue
+            #     penalty = (
+            #         self.model_config.penalties.user_constraint.request.hard
+            #         if r.hard
+            #         else self.model_config.penalties.user_constraint.request.soft
+            #     )
+            #     cstr_vars: List[
+            #         cp_model.IntVar | cp_model._NotBooleanVariable
+            #     ] = [var]
+            #     if any(
+            #         # pylint: disable=protected-access
+            #         isinstance(var, cp_model._NotBooleanVariable)
+            #         for var in cstr_vars
+            #     ):
+            #         raise ValueError(
+            #             "Request constraints should be boolean variables"
+            #         )
+            #     var_name = build_var_name_constraint(
+            #         r, cstr_vars, ObjectiveCategory.REQUEST  # type: ignore
+            #     )
+            #     # pylint: disable=R0801
+            #     lit = self.model.NewBoolVar(var_name)
+            #     if r.negative:
+            #         cstr_vars = [var.Not() for var in cstr_vars]
+            #     cstr_vars.append(lit)
+            #     self.model.AddBoolOr(cstr_vars)
+            #     self.obj.bool_vars.append(lit)
+            #     self.obj.bool_coeffs.append(penalty)
