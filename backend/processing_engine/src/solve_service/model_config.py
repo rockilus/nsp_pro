b3edde0329f12f8cd4588f5339c15553e52ad2f0
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-02-27 17:28:17 +0100

    Fix ObjectiveType in frontend.

--- backend/processing_engine/src/solve_service/model_config.py
+++ backend/processing_engine/src/solve_service/model_config.py
@@ -20,7 +20,7 @@ print(f"github_actions_mode: {github_actions_mode}")
 
 model_config = ModelConfig(
     solver_params=SolverParams(
-        max_time_in_seconds=90 if github_actions_mode else 30,
+        max_time_in_seconds=90 if github_actions_mode else 60,
         solve_strategy=SolveStrategy.HARD_TO_SOFT,
     ),
     configuration_constraints=ConfigurationConstraints(work_loads=False),
