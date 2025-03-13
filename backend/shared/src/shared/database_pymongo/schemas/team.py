8253a4737c17b361959e294092841da7fa79eeac
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-03-13 18:48:18 +0100

    Add team pymongo database.

---
+++ backend/shared/src/shared/database_pymongo/schemas/team.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from shared.database_pymongo.schemas.base import DocumentBaseSchema
+from shared.schemas.schemas.team import Team
+
+
+class TeamSchema(DocumentBaseSchema):
+    """Team schema for validation."""
+
+    team_members: List[str]
+    team_leaders: List[str]
+
+    def to_core(self) -> Team:
+        return Team(
+            id=self.id or "",
+            team_members=self.team_members,
+            team_leaders=self.team_leaders,
+        )
+
+    @classmethod
+    def from_core(cls, team: Team) -> "TeamSchema":
+        return cls(
+            id=team.id,
+            team_members=team.team_members,
+            team_leaders=team.team_leaders,
+        )
