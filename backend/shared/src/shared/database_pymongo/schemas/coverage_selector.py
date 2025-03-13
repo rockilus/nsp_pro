b452529fbe8d91a660a7367a6efb11a0a2a6b9a9
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-03-13 13:53:32 +0100

    Add coverage selector pymongo database.

---
+++ backend/shared/src/shared/database_pymongo/schemas/coverage_selector.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+from shared.database_pymongo.schemas.base import DocumentBaseSchema
+from shared.schemas.schemas.coverage import CoverageSelector as CoreCoverageSelector
+
+
+class CoverageSelectorSchema(DocumentBaseSchema):
+    """Coverage Selector schema for validation."""
+
+    schedule: str
+    coverage: Optional[str] = None
+    full_period: bool
+    start_date: datetime
+    end_date: datetime
+
+    def to_core(self) -> CoreCoverageSelector:
+        return CoreCoverageSelector(
+            id=self.id or "",
+            schedule_id=self.schedule,
+            coverage_id=self.coverage,
+            full_period=self.full_period,
+            start_date=self.start_date.date(),
+            end_date=self.end_date.date(),
+        )
+
+    @classmethod
+    def from_core(
+        cls, coverage_selector: CoreCoverageSelector
+    ) -> "CoverageSelectorSchema":
+        return cls(
+            id=coverage_selector.id,
+            schedule=coverage_selector.schedule_id,
+            coverage=coverage_selector.coverage_id,
+            full_period=coverage_selector.full_period,
+            start_date=datetime.combine(
+                coverage_selector.start_date,
+                datetime.min.time(),
+                tzinfo=timezone.utc,
+            ),
+            end_date=datetime.combine(
+                coverage_selector.end_date,
+                datetime.min.time(),
+                tzinfo=timezone.utc,
+            ),
+        )
