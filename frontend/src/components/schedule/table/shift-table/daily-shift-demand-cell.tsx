07242d178673f4e6ca1f5b3e17df83ca3af37513
Felipe Kharaba <felipekharaba@Felipes-MacBook-Pro.local>
2025-04-29 19:48:49 +0200

    Format background and text color in daily shift demand cell.

--- frontend/src/components/schedule/table/shift-table/daily-shift-demand-cell.tsx
+++ frontend/src/components/schedule/table/shift-table/daily-shift-demand-cell.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./daily-shift-demand-cell.css";
 // Types
 import { ScheduleCellDataT } from "../../../../types/schedule";
+// Constants
+import { TrafficLightColorMappings } from "../../../../constants/constants";
 
 export default function DailyShiftDemandCell({
   scheduleCellData,
@@ -18,10 +20,19 @@ export default function DailyShiftDemandCell({
       0
     ) || 0;
 
+  const { background, text } =
+    countActual === countTarget
+      ? TrafficLightColorMappings.green
+      : TrafficLightColorMappings.red;
+
   return (
     <div
       className="dsd-cell-container"
       onClick={() => handleDemandSelection(scheduleCellData)}
+      style={{
+        "--bg-color": background,
+        "--text-color": text,
+      } as React.CSSProperties}
     >
       <div className="dsd-cell-stats">
         <span className="dsd-stats dsd-stats-actual">{countActual}</span>
--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,3 +1,22 @@
+import {
+  amber,
+  blue,
+  brown,
+  cyan,
+  deepOrange,
+  deepPurple,
+  green,
+  indigo,
+  lightBlue,
+  lightGreen,
+  lime,
+  orange,
+  pink,
+  purple,
+  red,
+  teal,
+  yellow,
+} from "@mui/material/colors";
 import { ConstraintColorsT } from "../types/constraint";
 
 // General
@@ -15,6 +34,27 @@ export const NumHoursInDay = 24;
 export const NumQuarterHoursInHour = 4;
 export const PostSignInRoute = "/plan/workers";
 
+export const TrafficLightColorMappings: Record<
+  string,
+  { background: string; sample: string; text: string }
+> = {
+  red: {
+    background: red[100],
+    sample: red[500],
+    text: red[900],
+  },
+  amber: {
+    background: amber[100],
+    sample: amber[500],
+    text: amber[900],
+  },
+  green: {
+    background: green[100],
+    sample: green[500],
+    text: green[900],
+  },
+};
+
 // Shifts
 // source: https://mui.com/material-ui/customization/color/
 // 14 options, not using red, green, orange, and the greys
@@ -23,74 +63,74 @@ export const ShiftColorMappings: Record<
   { background: string; sample: string; text: string }
 > = {
   pink: {
-    background: "#f8bbd0",
-    sample: "#e91e63",
-    text: "#880e4f",
+    background: pink[100],
+    sample: pink[500],
+    text: pink[900],
   },
   purple: {
-    background: "#e1bee7",
-    sample: "#9c27b0",
-    text: "#4a148c",
+    background: purple[100],
+    sample: purple[500],
+    text: purple[900],
   },
   deepPurple: {
-    background: "#d1c4e9",
-    sample: "#673ab7",
-    text: "#311b92",
+    background: deepPurple[100],
+    sample: deepPurple[500],
+    text: deepPurple[900],
   },
   indigo: {
-    background: "#c5cae9",
-    sample: "#3f51b5",
-    text: "#1a237e",
+    background: indigo[100],
+    sample: indigo[500],
+    text: indigo[900],
   },
   blue: {
-    background: "#bbdefb", // 100
-    sample: "#2196f3", // 500
-    text: "#0d47a1", //900
+    background: blue[100],
+    sample: blue[500],
+    text: blue[900],
   },
   lightBlue: {
-    background: "#b3e5fc",
-    sample: "#03a9f4",
-    text: "#01579b",
+    background: lightBlue[100],
+    sample: lightBlue[500],
+    text: lightBlue[900],
   },
   cyan: {
-    background: "#b2ebf2",
-    sample: "#00bcd4",
-    text: "#006064",
+    background: cyan[100],
+    sample: cyan[500],
+    text: cyan[900],
   },
   teal: {
-    background: "#b2dfdb",
-    sample: "#009688",
-    text: "#004d40",
+    background: teal[100],
+    sample: teal[500],
+    text: teal[900],
   },
   lightGreen: {
-    background: "#dcedc8",
-    sample: "#8bc34a",
-    text: "#33691e",
+    background: lightGreen[100],
+    sample: lightGreen[500],
+    text: lightGreen[900],
   },
   lime: {
-    background: "#f0f4c3",
-    sample: "#cddc39",
-    text: "#827717",
+    background: lime[100],
+    sample: lime[500],
+    text: lime[900],
   },
   yellow: {
-    background: "#fff9c4",
-    sample: "#ffeb3b",
-    text: "#f57f17",
+    background: yellow[100],
+    sample: yellow[500],
+    text: yellow[900],
   },
-  amber: {
-    background: "#ffecb3",
-    sample: "#ffc107",
-    text: "#ff6f00",
+  orange: {
+    background: orange[100],
+    sample: orange[500],
+    text: orange[900],
   },
   deepOrange: {
-    background: "#ffccbc",
-    sample: "#ff5722",
-    text: "#bf360c",
+    background: deepOrange[100],
+    sample: deepOrange[500],
+    text: deepOrange[900],
   },
   brown: {
-    background: "#d7ccc8",
-    sample: "#795548",
-    text: "#3e2723",
+    background: brown[100],
+    sample: brown[500],
+    text: brown[900],
   },
 };
 
