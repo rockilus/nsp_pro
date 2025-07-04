b86c6491fbe550bbb4ea20f41432473ac02e23c0
Felipe Kharaba <felipe.kharaba@icloud.com>
2025-07-04 19:14:38 +0200

    fix: update version to 0.1.307 in poetry.lock

--- backend/shared/src/shared/schemas/core/schedule.py
+++ backend/shared/src/shared/schemas/core/schedule.py
@@ -81,27 +81,17 @@ class Schedule:
         return cls(
             id=data["id"],
             team_id=data["team_id"],
-            start_date=datetime.fromtimestamp(
-                data["start_date"], timezone.utc
-            ).date(),
-            end_date=datetime.fromtimestamp(
-                data["end_date"], timezone.utc
-            ).date(),
+            start_date=datetime.fromtimestamp(data["start_date"], timezone.utc).date(),
+            end_date=datetime.fromtimestamp(data["end_date"], timezone.utc).date(),
             status=ScheduleStatus(data["status"]),
             missing_coverage_dates=[
                 datetime.fromtimestamp(ts, timezone.utc).date()
                 for ts in data["missing_coverage_dates"]
             ],
             constraint_build_ids=data["constraint_build_ids"],
-            quick_staffings=[
-                QuickStaffing(**qs) for qs in data["quick_staffings"]
-            ],
-            created_at=datetime.fromtimestamp(
-                data["created_at"], timezone.utc
-            ),
-            updated_at=datetime.fromtimestamp(
-                data["updated_at"], timezone.utc
-            ),
+            quick_staffings=[QuickStaffing(**qs) for qs in data["quick_staffings"]],
+            created_at=datetime.fromtimestamp(data["created_at"], timezone.utc),
+            updated_at=datetime.fromtimestamp(data["updated_at"], timezone.utc),
             created_by=data["created_by"],
         )
 
@@ -185,9 +175,7 @@ class WorkTimeTable:
         data_snake = humps.decamelize(data.model_dump())
         data_snake["duties"] = WorkTimeTableData.from_dto(data_snake["duties"])
         data_snake["others"] = WorkTimeTableData.from_dto(data_snake["others"])
-        data_snake["workers"] = WorkTimeTableData.from_dto(
-            data_snake["workers"]
-        )
+        data_snake["workers"] = WorkTimeTableData.from_dto(data_snake["workers"])
         return WorkTimeTable(**data_snake)
 
 
@@ -276,9 +264,7 @@ class DuplicateResult:
 
     def to_dto(self) -> DuplicateResultDTO:
         data = asdict(self)
-        data["assignments"] = (
-            self.assignments.to_dto() if self.assignments else None
-        )
+        data["assignments"] = self.assignments.to_dto() if self.assignments else None
         data["demands"] = self.demands.to_dto() if self.demands else None
         as_dict = humps.camelize(data)
         validator = TypeAdapter(DuplicateResultDTO)
