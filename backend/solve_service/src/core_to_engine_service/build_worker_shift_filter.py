5d1e50b76785cac4d4a36cd01544dc747cfe426a
Felipe Kharaba <felipe.kharaba@icloud.com>
2025-11-11 15:39:26 +0100

    Refactor worker shift filter functions to separate dimension entry and boolean logic, and combine their results in a new function.

--- backend/solve_service/src/core_to_engine_service/build_worker_shift_filter.py
+++ backend/solve_service/src/core_to_engine_service/build_worker_shift_filter.py
@@ -15,14 +15,13 @@ from shared.schemas.core import (
 
 
 # pylint: disable=too-many-locals, too-many-branches, too-many-arguments
-def build_worker_shift_filters(
+def build_worker_shift_filters_dim_entry(
     workers: List[Worker],
     worker_ids_to_worker_dates: Dict[str, WorkerDates],
     shifts: List[Shift],
     dimensions: List[Dimension],
     attributes: List[Attribute],
-    penalty: int,
-) -> Tuple[List[Tuple[str, str, str]], int]:
+) -> List[Tuple[str, str, str]]:
     out: Set[Tuple[str, str, str]] = set()
 
     # Step 1: Identify shared dimensions
@@ -163,7 +162,7 @@ def build_worker_shift_filters(
 
     # Return the list of unique (worker_id, invalid_shift_id) and
     # (invalid_worker_id, shift_id) tuples
-    return list(out), penalty
+    return list(out)
 
 
 def build_worker_shift_filters_bool(
@@ -172,8 +171,7 @@ def build_worker_shift_filters_bool(
     shifts: List[Shift],
     dimensions: List[Dimension],
     attributes: List[Attribute],
-    penalty: int,
-) -> Tuple[List[Tuple[str, str, str]], int]:
+) -> List[Tuple[str, str, str]]:
     """Build worker-shift filters for boolean-type shared dimensions only.
 
     Returns list of (worker_id, date_iso, shift_id) tuples and penalty.
@@ -296,220 +294,33 @@ def build_worker_shift_filters_bool(
                         ]
                     )
 
-    return list(out), penalty
+    return list(out)
 
 
-# from collections import defaultdict
-# from typing import Dict, List, Set, Tuple
+def build_worker_shift_filters(
+    workers: List[Worker],
+    worker_ids_to_worker_dates: Dict[str, WorkerDates],
+    shifts: List[Shift],
+    dimensions: List[Dimension],
+    attributes: List[Attribute],
+    penalty: int,
+) -> Tuple[List[Tuple[str, str, str]], int]:
+    """Combine filters from dim-entry and bool shared dimensions.
 
-# from shared.schemas.core import (
-#     Attribute,
-#     AttributeOwnerType,
-#     Dimension,
-#     DimensionEntryType,
-#     DimensionType,
-#     Shift,
-#     ShiftType,
-#     Worker,
-#     WorkerDates,
-# )
+    This function delegates to build_worker_shift_filters_dim_entry and
+    build_worker_shift_filters_bool, merges their results (deduplicated)
+    and returns the combined list of (worker_id, date_iso, shift_id)
+    tuples along with a penalty. The returned penalty is the max of the
+    two returned penalties (they usually receive the same `penalty`).
+    """
+    dim_out = build_worker_shift_filters_dim_entry(
+        workers, worker_ids_to_worker_dates, shifts, dimensions, attributes
+    )
 
+    bool_out = build_worker_shift_filters_bool(
+        workers, worker_ids_to_worker_dates, shifts, dimensions, attributes
+    )
 
-# # pylint: disable=too-many-locals, too-many-branches, too-many-arguments
-# # pylint: disable=too-many-statements, too-many-positional-arguments
-# def build_worker_shift_filters(
-#     workers: List[Worker],
-#     worker_ids_to_worker_dates: Dict[str, WorkerDates],
-#     shifts: List[Shift],
-#     dimensions: List[Dimension],
-#     attributes: List[Attribute],
-#     penalty: int,
-# ) -> Tuple[List[Tuple[str, str, str]], int]:
-#     out: Set[Tuple[str, str, str]] = set()
+    combined_set = set(dim_out) | set(bool_out)
 
-#     # Step 1: Identify shared dimensions
-#     shared_dimensions = [
-#         dim
-#         for dim in dimensions
-#         if not dim.deleted
-#         and (
-#             DimensionType.WORKER in dim.dim_types
-#             and DimensionType.SHIFT in dim.dim_types
-#         )
-#         or (
-#             DimensionType.WORKER in dim.dim_types
-#             and DimensionType.REST_SHIFT in dim.dim_types
-#         )
-#     ]
-#     dim_by_ids = {dim.id: dim for dim in shared_dimensions}
-#     shared_dim_ids = {dim.id for dim in shared_dimensions}
-
-#     # Step 2: Build attribute mappings
-#     # Worker attributes: worker_id -> dimension_id -> dim_entry_ids
-#     worker_attrs: Dict[str, Dict[str, Set[str]]] = defaultdict(lambda:
-# defaultdict(set))
-#     # Worker boolean attributes: worker_id -> dimension_id -> bool
-#     worker_bool_attrs: Dict[str, Dict[str, bool]] = defaultdict(dict)
-#     # Shift attributes: shift_id -> dimension_id -> dim_entry_ids
-#     shift_attrs: Dict[str, Dict[str, Set[str]]] = defaultdict(lambda:
-# defaultdict(set))
-#     # Shift boolean attributes: shift_id -> dimension_id -> bool
-#     shift_bool_attrs: Dict[str, Dict[str, bool]] = defaultdict(dict)
-
-#     dim_id_to_attrs: Dict[str, List[Attribute]] = defaultdict(list)
-#     for attr in attributes:
-#         if attr.dimension_id in shared_dim_ids:
-#             dim_id_to_attrs[attr.dimension_id].append(attr)
-
-#     for attr in attributes:
-#         dimension = dim_by_ids.get(attr.dimension_id, None)
-#         if dimension is None:
-#             continue
-#         if attr.owner_type == AttributeOwnerType.WORKER:
-#             if dimension.entry_type == DimensionEntryType.DIM_ENTRIES:
-#                 for de_id in attr.dim_entry_ids:
-#                     worker_attrs[attr.owner_id][attr.dimension_id].add(de_id)
-#             elif dimension.entry_type == DimensionEntryType.BOOL and isinstance(
-#                 attr.value, bool
-#             ):
-#                 # store actual bool value in a separate map
-#                 worker_bool_attrs[attr.owner_id][attr.dimension_id] = bool(attr.value)
-#         elif attr.owner_type == AttributeOwnerType.SHIFT:
-#             if dimension.entry_type == DimensionEntryType.DIM_ENTRIES:
-#                 for de_id in attr.dim_entry_ids:
-#                     shift_attrs[attr.owner_id][attr.dimension_id].add(de_id)
-#             elif dimension.entry_type == DimensionEntryType.BOOL and isinstance(
-#                 attr.value, bool
-#             ):
-#                 # store actual bool value in a separate map
-#                 shift_bool_attrs[attr.owner_id][attr.dimension_id] = bool(attr.value)
-
-#     # Map to group workers by their invalid_shift_ids
-#     all_worker_ids: Set[str] = {worker.id for worker in workers if not worker.deleted}
-
-#     # Step 3: Build worker shift filters
-#     for dimension in shared_dimensions:
-#         dimension_id = dimension.id
-
-#         # Determine the shift types to include based on dimension.dim_types
-#         shift_types_to_include = set()
-
-#         if DimensionType.SHIFT in dimension.dim_types:
-#             shift_types_to_include.update({ShiftType.NORMAL, ShiftType.DUTY})
-
-#         if DimensionType.REST_SHIFT in dimension.dim_types:
-#             shift_types_to_include.add(ShiftType.REST)
-
-#         # Build relevant_shift_ids accordingly
-#         relevant_shift_ids: Set[str] = {
-#             shift.id
-#             for shift in shifts
-#             if not shift.deleted and shift.shift_type in shift_types_to_include
-#         }
-
-#         # -------- Filter out shifts that don't have worker attribute --------
-
-#         # Map of attribute value to shift IDs (for DIM_ENTRIES)
-#         attr_value_to_shift_ids: Dict[str, Set[str]] = defaultdict(set)
-#         for shift_id, attrs in shift_attrs.items():
-#             if dimension_id in attrs:
-#                 attr_dim_entry_ids = attrs[dimension_id]
-#                 for de_id in attr_dim_entry_ids:
-#                     attr_value_to_shift_ids[de_id].add(shift_id)
-
-#         # Map of boolean attribute value to shift IDs (for BOOL dimensions)
-#         attr_bool_value_to_shift_ids: Dict[bool, Set[str]] = defaultdict(set)
-#         for shift_id, bool_attrs in shift_bool_attrs.items():
-#             if dimension_id in bool_attrs:
-#                 attr_bool_value_to_shift_ids[bool_attrs[dimension_id]].add(shift_id)
-
-#         for worker in workers:
-#             if worker.id in worker_attrs and dimension_id in worker_attrs[worker.id]:
-#                 worker_attr_dim_entry_ids = worker_attrs.get(worker.id, {}).get(
-#                     dimension_id, set()
-#                 )
-
-#                 valid_shift_ids = set()
-#                 # matches from DIM_ENTRIES
-#                 for de_id in worker_attr_dim_entry_ids:
-#                     valid_shift_ids.update(attr_value_to_shift_ids.get(de_id, set()))
-
-#                 # matches from BOOL attributes (if any)
-#                 if (
-#                     worker.id in worker_bool_attrs
-#                     and dimension_id in worker_bool_attrs[worker.id]
-#                 ):
-#                     worker_bool_value = worker_bool_attrs[worker.id][dimension_id]
-#                     bool_matches = attr_bool_value_to_shift_ids.get(
-#                         worker_bool_value, set()
-#                     )
-#                     valid_shift_ids.update(bool_matches)
-
-#                 # Shifts that do not have the same attribute value
-#                 invalid_shift_ids = relevant_shift_ids - valid_shift_ids
-
-#                 # Add invalid tuples for the worker
-#                 for shift_id in invalid_shift_ids:
-#                     out.update(
-#                         [
-#                             (worker.id, d.isoformat(), shift_id)
-#                             for d in worker_ids_to_worker_dates[
-#                                 worker.id
-#                             ].dates_campaign
-#                         ]
-#                     )
-
-#         # -------- Filter out workers that don't have shift attribute --------
-
-#         # Map of attribute value to worker IDs (for DIM_ENTRIES)
-#         attr_value_to_worker_ids: Dict[str, Set[str]] = defaultdict(set)
-#         for worker_id, attrs in worker_attrs.items():
-#             if dimension_id in attrs:
-#                 attr_dim_entry_ids = attrs[dimension_id]
-#                 for de_id in attr_dim_entry_ids:
-#                     attr_value_to_worker_ids[de_id].add(worker_id)
-
-#         # Map of boolean attribute value to worker IDs (for BOOL dimensions)
-#         attr_bool_value_to_worker_ids: Dict[bool, Set[str]] = defaultdict(set)
-#         for worker_id, bool_attrs in worker_bool_attrs.items():
-#             if dimension_id in bool_attrs:
-#                 attr_bool_value_to_worker_ids[bool_attrs[dimension_id]].add(worker_id)
-
-#         for shift in [s for s in shifts if not s.deleted]:  # [TO REVIEW]
-#             # workers that match this shift (DIM_ENTRIES and BOOL)
-#             valid_worker_ids = set()
-#             if shift.id in shift_attrs and dimension_id in shift_attrs[shift.id]:
-#                 shift_attr_dim_entry_ids = shift_attrs.get(shift.id, {}).get(
-#                     dimension_id, set()
-#                 )
-#                 for de_id in shift_attr_dim_entry_ids:
-#                     valid_worker_ids.update(attr_value_to_worker_ids.get(
-# de_id, set()))
-
-#             # BOOL-matching
-#             if (
-#                 shift.id in shift_bool_attrs
-#                 and dimension_id in shift_bool_attrs[shift.id]
-#             ):
-#                 shift_bool_value = shift_bool_attrs[shift.id][dimension_id]
-#                 valid_worker_ids.update(
-#                     attr_bool_value_to_worker_ids.get(shift_bool_value, set())
-#                 )
-
-#                 # Shifts that do not have the same attribute value
-#                 invalid_worker_ids = all_worker_ids - valid_worker_ids
-
-#                 # Add invalid tuples for the worker
-#                 for worker_id in invalid_worker_ids:
-#                     out.update(
-#                         [
-#                             (worker_id, d.isoformat(), shift.id)
-#                             for d in worker_ids_to_worker_dates[
-#                                 worker_id
-#                             ].dates_campaign
-#                         ]
-#                     )
-
-#     # Return the list of unique (worker_id, invalid_shift_id) and
-#     # (invalid_worker_id, shift_id) tuples
-#     return list(out), penalty
+    return list(combined_set), penalty
