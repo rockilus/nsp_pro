ed58356d753d1253cb62981593a1b734a2665f4d
Felipe Kharaba <felipe.kharaba@icloud.com>
2025-11-20 11:58:06 +0100

    Refactor conditional statement for better readability in build_breaches function and update monthly target duties in penalties

--- backend/solve_service/src/solve_service/penalties.py
+++ backend/solve_service/src/solve_service/penalties.py
@@ -33,7 +33,7 @@ penalties_prod = Penalties(
     ),
     system_constraint=SystemConstraintPenalty(
         weekly_target_work_time=5,
-        monthly_target_nb_duties=1,
+        monthly_target_nb_duties=100,
         special_days_target_nb_duties=1,
     ),
 )
