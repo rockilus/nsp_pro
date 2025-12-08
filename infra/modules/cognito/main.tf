c4a805ba1180fc8218e2a0eee5ca28a51a78d3c9
Felipe Kharaba <felipe.kharaba@icloud.com>
2025-12-08 22:47:44 +0100

    Update Cognito configuration to include admin scope for OAuth

--- infra/modules/cognito/main.tf
+++ infra/modules/cognito/main.tf
@@ -169,7 +169,7 @@ resource "aws_cognito_user_pool_client" "main" {
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_flows_user_pool_client = true
   # allowed_oauth_scopes                 = ["email", "openid", "profile"]
-  allowed_oauth_scopes         = ["email", "openid", "phone"]
+  allowed_oauth_scopes         = ["email", "openid", "phone", "aws.cognito.signin.user.admin"]
   callback_urls                = ["https://${var.frontend_domain_name}/fr/plan/workers/"]
   logout_urls                  = ["https://${var.landing_page_domain_name}"]
   supported_identity_providers = ["COGNITO"]
