fdcea85e14d56a742ea536b506268ffba0fc58fc
Felipe Kharaba <felipe.kharaba@icloud.com>
2026-03-04 16:55:17 +0100

    Add X-Impersonation-Token to CORS headers in API Gateway integration response

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -222,7 +222,7 @@ resource "aws_api_gateway_integration_response" "options_proxy" {
   http_method = aws_api_gateway_method.options_proxy.http_method
   status_code = aws_api_gateway_method_response.options_proxy.status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,fdi-version,rid,st-auth-mode'"
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,fdi-version,rid,st-auth-mode,X-Impersonation-Token'"
     "method.response.header.Access-Control-Allow-Methods"     = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
     "method.response.header.Access-Control-Allow-Origin"      = "'${join(",", var.cors_allowed_origins)}'"
     "method.response.header.Access-Control-Allow-Credentials" = "'true'"
