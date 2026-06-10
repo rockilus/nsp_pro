d26520319bfceab5a5c2fc60624e21408891126d
Felipe Kharaba <felipe.kharaba@icloud.com>
2026-06-10 17:55:20 +0200

    chore(eslint): disable strict react-hooks rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,14 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+    {
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/refs': 'off',
+      'react-hooks/preserve-manual-memoization': 'off',
+      'react-hooks/immutability': 'off',
+    },
+  },
 ]);
 
 export default eslintConfig;
